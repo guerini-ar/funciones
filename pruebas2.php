35f6f647da522f880e511f1755fdb4af084ed93d
Gerardo A. Guerini <44976268+guerini-ar@users.noreply.github.com>
2018-12-11 21:04:24 -0300

    Add files via upload

---
+++ pruebas2.php
@@ -0,0 +1,35 @@
+<?php
+
+// Hacer una función para mostrar una sucesión de números
+
+function secuenciaNumerica($a, $b){
+    
+    $sec = "";
+
+    if($a < $b){
+        /*Secuencia ascendente*/
+        echo 'Secuencia numerica ascendente:'.'<br/>';
+        for ($a=$a; $a <= $b; $a++) { 
+            $sec .= $a.', ';
+        }
+    }elseif($a > $b){
+        /*Secuencia descendente*/
+        echo 'Secuencia numerica descendente:'.'<br/>';
+        for ($a=$a; $a > $b; $a--) { 
+            $sec .= $a.', ';
+        }
+    }else{
+        echo 'Ambos numeros son iguales.';
+    }
+
+    return $sec;
+}
+
+if(isset($_GET['n1']) && isset($_GET['n2'])){
+    echo secuenciaNumerica($_GET['n1'], $_GET['n2']);
+}else{
+    echo 'No se ingresaron numeros por el metodo GET';
+}
+
+
+?>
